47d38f02487c686779df520404b1db2245f6635b
Uran Hajrizaj <uranhajrizaj@outlook.com>
2022-08-19 14:14:59 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,108 +3,86 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const[squares, setSquares] = useState([
-    {
-    id: 11,
-    checked: false,
-    value: "",
-  
-   },
-   {
-    id: 12,
-    checked: false,
-    value: "",
+  const[board, setBoard] = useState([
+    [
+      {  value: "" },
+      { value: '' },
+      { value: '' }
 
+    ],
+    [ { value: "" },
+      { value: '' },
+      {value: '' }],
+    [ {  value: "" },
+      {  value: '' },
+      {  value: '' }]
+  ])
+  const[playerX, setPlayerX] = useState(true)
+  const[winner, setWinner] = useState("")
+  function clickCell (rowIndex: number, cellIndex: number) {
+    const boardCopy = structuredClone(board)
+    boardCopy[rowIndex][cellIndex].value = playerX ?"X":"O"
+    setBoard(boardCopy)
+  }
+
+   function checkWinner () {
+     const boardCopy = structuredClone(board)
+     const winner = checkRows(boardCopy) || checkColumns(boardCopy) || checkDiagonals(boardCopy)
+     setWinner(winner) 
+   }
+    function checkRows (board: any[][]) {
+      for (let row of board) {
+        if (row.every(cell => cell.value === "X")) {
+          return "X"
+        } else if (row.every(cell => cell.value === "O")) {
+          return "O"
+        }
+      }
+      return null
+    }
+    function checkColumns (board: any[][]) {
+      for (let i = 0; i < board.length; i++) {
+        const column = board.map(row => row[i])
+        if (column.every(cell => cell.value === "X")) {
+          return "X"
+        } else if (column.every(cell => cell.value === "O")) {
+          return "O"
+        }
+      }
+      return null
+    }
+    function checkDiagonals (board: any[][]) {
+      const diagonal1 = board.map((row, index) => row[index])
+      const diagonal2 = board.map((row, index) => row[row.length - 1 - index])
+      if (diagonal1.every(cell => cell.value === "X")) {
+        return "X"
+      } else if (diagonal1.every(cell => cell.value === "O")) {
+        return "O"
+      } else if (diagonal2.every(cell => cell.value === "X")) {
+        return "X"
+      } else if (diagonal2.every(cell => cell.value === "O")) {
+        return "O"
+      }
+      return null
+    }
     
-   },
-   {
-    id: 13,
-    checked: false,
-    value: "",
-   
-    
-   },
-   {
-    id: 21,
-    checked: false,
-    value: "",
-
-   
-   
-   },
-  
-   {
-    id: 22,
-    checked: false,
-    value: "",
-
-   
-   },
-   {
-    id: 23,
-    checked: false,
-    value: "",
-  
-   },
-   {
-    id: 31,
-    checked: false,
-    value: "",
-   
-   },
-   {
-    id: 32,
-    checked: false,
-    value: "",
-   },
-   {
-    id: 33,
-    checked: false,
-    value: "",
-  
-   }])
-
- const [displayX, setDisplayX] = useState(false)
-
-  const [winnerX, setWinnerX] = useState(false)
-  const [winnerO, setWinnerO] = useState(false)
-
-  const[numberO, setNumberO] = useState(0)
-  const[numberX, setNumberX] = useState(0)
-
-
-   function update(target){
-    let updatesquares= squares.map(square => target.id===square.id ? {...square, checked: true, value: displayX ? "X":"O"} : square)
-    setSquares(updatesquares)
-    console.log(updatesquares)
-    
-    
-
-
-
-}
-
-
 
   return (
     <div className="App">
-       <div className='tabel'>
-       <ul>
-       {squares.map(square => (
-            <>
-            <input onClick={()=>
-            {update(square)
-            setDisplayX(!displayX)
-        }}type="checkbox" name="myCheckboxes" id={'square.id'} checked={square.checked}/>
-            {square.checked ? <label>{square.value}</label>: null}
-          
-            </>
-        ))}
-          {winnerX?<p>You Win</p>:null}
-          {/* {winnerO?<p>You Win</p>:null} */}
-       </ul>
-      
+       <div className='board'>
+         {board.map((row, rowIndex) => {
+            return row.map((cell, cellIndex) => {
+              return <div className='cell' onClick={()=>{
+                setPlayerX(!playerX)
+                clickCell (rowIndex, cellIndex)
+                checkWinner ()
+              }}>{cell.value}</div>
+            })
+         }
+          )}
+        
       </div>
+      {winner && <div className='winner'><h1>Player {winner} win the game</h1></div>}
     </div>
   )
 }
